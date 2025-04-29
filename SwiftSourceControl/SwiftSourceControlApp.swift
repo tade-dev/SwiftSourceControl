acfe33038e95a8c515deaeef63637878312dfb0d
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-04-29 17:37:01 +0100

    Initial Commit

---
+++ SwiftSourceControl/SwiftSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlApp.swift
+//  SwiftSourceControl
+//
+//  Created by BSTAR on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftSourceControlTests/SwiftSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourceControlTests.swift
+//  SwiftSourceControlTests
+//
+//  Created by BSTAR on 29/04/2025.
+//
+
+import Testing
+@testable import SwiftSourceControl
+
+struct SwiftSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
